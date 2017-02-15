3562fa63a1d63a958a57b314a35437af2e57d3e5
Bernardo Siqueira <bernardofcs@hotmail.com>
2017-02-15 14:58:25 +0000

    added packages and initial server

---
+++ app.js
@@ -0,0 +1,16 @@
+const router = require('./routes');
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+router(app);
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+})
+
